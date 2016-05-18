97879cd3dab91166285281c14977115577c3c6ce
fengyeqizhou <dongbeibei_yun@163.com>
2016-05-18 19:51:51 +0800

    Create new shell script

---
+++ shell/8/log.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "who"
+who
+
+echo
+echo
+echo "w"
+w
+
+echo
+echo
+echo "users"
+users
+
+echo
+echo
+echo "uptime"
+uptime
+
+echo
+echo
+echo "last"
+last
+
+echo
+echo
+echo "last reboot"
+last reboot
