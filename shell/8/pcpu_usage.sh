d37f9ee87435959de5bd1b63ca3afc6d84e9f79f
fengyeqizhou <dongbeibei_yun@163.com>
2016-05-18 19:52:52 +0800

    Create new shell script

---
+++ shell/8/pcpu_usage.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SECS=3600
+UNIT_TIME=60
+
+STEPS=$(( $SECS / $UNIT_TIME ))
+
+echo Watching CPU usage... ;
+
+for ((i=0 ; i<STEPS ; i++))
+do
+	ps -eo comm,pcpu | tail -n +2 >> /tmp/cpu_usage.$$
+	sleep $UNIT_TIME
+done
+
+echo
+echo CPU eaters:
+
+cat /tmp/cpu_usage.$$ | \
+awk '
+{ process[$1]+=$2; }
+END {
+for (i in process)
+{
+    printf("%-20s %s", i, process[i]);
+}
+}' | sort -nrk 2 | head
+rm /tmp/cpu_usage.$$
