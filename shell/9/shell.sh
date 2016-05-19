0e722b42b6d3e6173ec17328a482ef4310334479
fengyeqizhou <dongbeibei_yun@163.com>
2016-05-19 11:47:52 +0800

    Create new shell script

---
+++ shell/9/shell.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "which ls"
+which ls
+
+echo
+echo "whereis ls"
+whereis ls
+
+echo
+echo "file /bin/ls"
+file /bin/ls
+
+#echo
+#echo "whatis ls"
+#whatis ls
+
+echo
+echo "uptime"
+uptime
